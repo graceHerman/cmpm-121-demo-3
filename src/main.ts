7dd94b8fc8c4fb5e6490fc8ceedf1c70c2ea9731
graceHerman <151581400+graceHerman@users.noreply.github.com>
2024-11-02 12:23:26 -0700

    Added button to main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 // todo
+
+// @deno-types="npm:@types/leaflet@^1.9.14"
+//import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+//import luck from "./luck.ts";
+
+const button = document.createElement("button");
+button.innerHTML = "Click here";
+
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+
+document.body.appendChild(button);
